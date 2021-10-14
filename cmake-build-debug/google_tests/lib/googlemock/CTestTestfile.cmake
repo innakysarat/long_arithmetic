fee8a74f79ff609ad8b7b3e3598c2ca86d976e43
Inna <kysarat@gmail.com>
2021-10-14 16:31:48 +0300

    remove debug

--- cmake-build-debug/google_tests/lib/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/kysar/CLionProjects/long_arithmetic/google_tests/lib/googlemock
-# Build directory: /mnt/c/Users/kysar/CLionProjects/long_arithmetic/cmake-build-debug/google_tests/lib/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
